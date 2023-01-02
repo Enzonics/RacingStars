38e2e1cfebcdc8dfaa7045b1c1df52abdf24e911
Enzo Onyx <enzo@testing.comgit config --global user.email enzo@testing.com>
2023-01-02 01:35:39 +0000

    functions work, calculation works, need to add css and design!

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+
+let day;
+let month;
+let year;
+
+let today = new Date;
+
+
+
+function birthdayObtain(){
+  day = document.getElementById("birthDay").value;
+  month = document.getElementById("birthMonth").value
+  year = document.getElementById("birthYear").value
+  birthdayCalc();
+}
+
+function birthdayCalc() {
+
+  var userBirthday = new Date(today.getFullYear(), month, day)
+  console.log(userBirthday)
+
+  if (today.getMonth() === month && today.getDate() > day) 
+  userBirthday.setFullYear(userBirthday.getFullYear() + 1) 
+  
+  var diff_in_time = userBirthday.getTime() - today.getTime();
+  var diff_in_days = diff_in_time / (1000*3600*24)
+
+ 
+  var final_result = diff_in_days.toFixed(0)
+  console.log(final_result)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <text> Birthday Calculator </text>
+      <text> Please enter your birthday </text>
+      <input placeholder='day' type='string' id='birthDay' />
+      <input placeholder='month' type='string' id='birthMonth' />
+      <input placeholder='year' type='string' id='birthYear' />
+
+      <button onClick={birthdayObtain}> Calculate </button>
     </div>
   );
 }
