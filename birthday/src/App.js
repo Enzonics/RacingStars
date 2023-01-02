11ccf66380f75d31c238b3dc356fa7d8d2fd4cf7
Enzo Onyx <enzo@testing.comgit config --global user.email enzo@testing.com>
2023-01-02 14:37:02 +0000

    displays dob and days until next birthday - next to add css and design

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -4,44 +4,60 @@ import './App.css'
 let day;
 let month;
 let year;
+let userDay;
 
 let today = new Date;
 
+let result;
 
 
-function birthdayObtain(){
+
+async function birthdayObtain(){
   day = document.getElementById("birthDay").value;
   month = document.getElementById("birthMonth").value
   year = document.getElementById("birthYear").value
+
+  userDay = day + '/' + month + '/' + year
+
   birthdayCalc();
 }
 
-function birthdayCalc() {
-
+async function birthdayCalc() {
   var userBirthday = new Date(today.getFullYear(), month, day)
-  console.log(userBirthday)
-
-  if (today.getMonth() === month && today.getDate() > day) 
-  userBirthday.setFullYear(userBirthday.getFullYear() + 1) 
+    if (today.getMonth() === month && today.getDate() > day) 
+      userBirthday.setFullYear(userBirthday.getFullYear() + 1) 
   
   var diff_in_time = userBirthday.getTime() - today.getTime();
   var diff_in_days = diff_in_time / (1000*3600*24)
 
  
-  var final_result = diff_in_days.toFixed(0)
-  console.log(final_result)
+  result = diff_in_days.toFixed(0)
+
+  mapData();
 }
+
+function mapData(){
+ document.getElementById('birthday').innerHTML = `Your birthday is ${userDay}`;
+ document.getElementById('daysUntil').innerHTML = `Your birthday is ${result} days`
+}
+
 
 function App() {
   return (
-    <div>
-      <text> Birthday Calculator </text>
-      <text> Please enter your birthday </text>
+    <div className='container'>
+      <div>
+        <text> Birthday Calculator </text>
+        <text> Please enter your birthday </text>
+      </div>
+      <div>
       <input placeholder='day' type='string' id='birthDay' />
       <input placeholder='month' type='string' id='birthMonth' />
       <input placeholder='year' type='string' id='birthYear' />
+      </div>
 
       <button onClick={birthdayObtain}> Calculate </button>
+      <text id='birthday'></text>
+      <text id='daysUntil'></text>
     </div>
   );
 }
--- birthday/tailwind.config.js
+++ birthday/tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: [],
   theme: {
     extend: {},
+    container: {
+      center: true
+      
+    },
+    colors: {
+      'grey': '#0681B1',
+      'white': '#FFFFFF'
+    }
   },
   plugins: [],
 }
