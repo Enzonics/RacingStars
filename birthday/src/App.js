e7a515158b011107361661443e22bbdd70b7e634
Enzo Onyx <enzo@testing.comgit config --global user.email enzo@testing.com>
2023-01-03 22:42:28 +0000

    final touches, and completion

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -16,7 +16,29 @@ async function birthdayObtain(){
 
   userDay = day + '/' + month + '/' + year
 
-  birthdayCalc();
+
+  switch (true) {
+    case day > 31:
+      document.getElementById('birthday').innerHTML = 'Invalid day, please try again'
+      document.getElementById('daysUntil').innerHTML = ''
+      break;
+    case month > 12:
+      document.getElementById('birthday').innerHTML = 'Invalid month, please try again'
+      document.getElementById('daysUntil').innerHTML = ''
+      break;
+    case month == 2:
+      birthdayCalc()
+      break;
+    case month == 2:
+    case day > 28:
+      document.getElementById('birthday').innerHTML = 'The Febuary Complex'
+      document.getElementById('daysUntil').innerHTML = 'Are you trying to trick me?'
+      break;
+    default:
+      birthdayCalc();
+    break;
+  }
+
 }
 
 async function birthdayCalc() {
@@ -26,7 +48,9 @@ async function birthdayCalc() {
   var diff_in_time = userBirthday.getTime() - today.getTime();
   var diff_in_days = diff_in_time / (1000*3600*24)
   result = diff_in_days.toFixed(0)
+
   mapData();
+  clearFields();
 }
 
 function mapData(){
@@ -34,31 +58,38 @@ function mapData(){
  document.getElementById('daysUntil').innerHTML = `Your birthday is ${result} days`
 }
 
+function clearFields() {
+  document.getElementById('birthDay').value = '';
+  document.getElementById('birthMonth').value = '';
+  document.getElementById('birthYear').value = '';
+}
+
+
 
 function App() {
   return (
     <div className='container bg-white'>
-      <div className='card bg-grey rounded-xl '>
+      <div className='card bg-grey rounded-xl'>
         <div>
             <h1 className="text-white text-3xl text-center"> Birthday Calculator </h1>
             <h2 className="text-white text-l text-center"> Please enter your birthday </h2>
-            <hr className='' />
+            <hr className=' ' />
         </div>
         
 
         <div className=' md:flex space-x-8 justify-center pt-5'>
-          <input  max='31' placeholder='day' type='number' id='birthDay' />
-          <input placeholder='month' type='number' id='birthMonth' />
-          <input placeholder='year' type='number' id='birthYear' />
+          <input  min='1' max='31' placeholder='day' type='number' id='birthDay' />
+          <input min='1' max='12' placeholder='month' type='number' id='birthMonth'  />
+          <input min='1900' max='2023' placeholder='year' type='number' id='birthYear' />
         </div>
 
         <div className=' md:flex justify-center pt-5' >
-          <button className='bg-white rounded-lg  ' onClick={birthdayObtain}> Calculate </button>
+          <button className='bg-white rounded-lg w-80 ' onClick={birthdayObtain}> Calculate </button>
         </div>
 
-        <div className='md:flex justify-center pt-5 pb-5'>
-         <text id='birthday'></text>
-         <text id='daysUntil'></text>  
+        <div className='flex flex-col justify-items-center pt-5 pb-5 '>
+         <text className='flex justify-center text-white' id='birthday'></text>
+         <text className='flex justify-center text-white'  id='daysUntil'></text>  
         </div>
       </div>
     </div>
